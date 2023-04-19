f7126d266bf6596e79f1937f8f49350e45b76ad6
xavierVinas <xavivinass@gmail.com>
2023-04-19 10:55:24 +0200

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     
     <div className="App">
       <header className="App-header">
-        <h1>hola mundo </h1>
+        <h1>hola mundo aloha  </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
